0ded486af6917ed31870ef21c170c08a9178d36c
kojiba <kojiba@ro.ru>
2015-05-18 12:25:44 +0300

    Added bgs. Changed hello screen, added LoginViewController.

---
+++ OpenPath/Classes/LoginViewController.m
@@ -0,0 +1,23 @@
+//
+// Created by Ilya Kucheruavyu on 5/18/15.
+// Copyright (c) 2015 Ilya Kucheruavyu. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+
+@implementation LoginViewController {
+
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
